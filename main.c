b02574ddc8fd2163673884c86066b4ac451ba2f5
atucci <alessiotucci33@gmail.com>
2024-04-27 14:31:45 +0200

    norminette is ok, working on parsing the map

--- main.c
+++ main.c
@@ -6,13 +6,16 @@
 /*   By: atucci <atucci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 14:18:53 by atucci            #+#    #+#             */
-/*   Updated: 2024/04/27 14:19:14 by atucci           ###   ########.fr       */
+/*   Updated: 2024/04/27 14:27:54 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-int	main()
+int	main(int ac, char **av)
 {
-	return (0);
+	if (ac == 2)
+		return (0);
+	else
+		return (-1 * ft_printf("%sWrong Usage:%s%s map\n", RED, RESET, av[0]));
 }
--- minirt.h
+++ minirt.h
@@ -6,7 +6,7 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 17:43:02 by atucci            #+#    #+#             */
-/*   Updated: 2024/04/27 14:16:16 by atucci           ###   ########.fr       */
+/*   Updated: 2024/04/27 14:30:49 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define MINIRT_H
 
 /************************/
-/* Define keyboard code */
+/* Define keyboard code */ //TODO: check with Ubuntu codes
 /************************/
 # define ARROW_LEFT 123
 # define ARROW_RIGHT 124
@@ -26,11 +26,38 @@
 # define KEY_R 15
 # define ESCAPE 53
 
+//TODO: CHECK UBUNTU CODES
+# define LEFT 65361
+# define RIGHT 65363
+# define W 119
+# define A 97
+# define S 115
+# define D 100
+# define ESC 65307
+# define NORTH_SOUTH 0
+# define EAST_WEST 1
+
+/* Define color macros */
+# define BLUE    "\033[1;34m"
+# define PURPLE  "\033[1;35m"
+# define GRAY    "\033[1;30m"
+# define GREEN   "\033[1;32m"
+# define CYAN    "\033[1;36m"
+# define YELLOW  "\033[1;33m"
+# define RED     "\033[1;31m"
+# define RESET   "\033[0m"
+/* Define Background colors macros */
+# define BG_RESET        "\033[49m"
+# define BG_GREEN        "\033[42m"
+# define BG_CYAN         "\033[46m"
+# define BG_YELLOW       "\033[43m"
+# define BG_RED          "\033[41m"
+
 /***************************************/
 /* Define default size for the windows */
 /***************************************/
 # define DEFAULT_WIDTH 1280
 # define DEFAULT_HEIGHT 720
 
-#include "./libft/libft.h"
+# include "./libft/libft.h"
 #endif
