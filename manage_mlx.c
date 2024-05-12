6c9e60c8f7ad0e8804bc42e8e408975f9e1361bc
Alessio Tucci <alessiotucci33@gmail.com>
2024-05-12 15:49:17 +0200

    play around with mlx, nothing good came out of it

--- manage_mlx.c
+++ manage_mlx.c
@@ -6,7 +6,7 @@
 /*   By: atucci <atucci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 15:39:26 by atucci            #+#    #+#             */
-/*   Updated: 2024/05/12 15:31:50 by atucci           ###   ########.fr       */
+/*   Updated: 2024/05/12 15:48:50 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,6 +71,47 @@ int	window_close(void *param)
 	return (0);
 }
 
+/*static void	my_test(t_mlx *obj, t_setting *set)
+{
+
+ // Iterate over each pixel
+	for (int y = 0; y < obj->height; y++)
+	{
+		for (int x = 0; x < obj->width; x++)
+		{
+			// The ray is defined by origin (O) and direction (dir)
+			t_vector O = set->camera->orientation;
+			t_vector dir = calculate_direction(set->camera, x, y); // You need to implement this function
+
+			// The sphere is defined by its center (C) and radius (r)
+			t_vector C = set->spheres[0]->center;
+			double r = set->spheres[0]->diameter / 2.0;
+
+			// Calculate the terms of the quadratic equation
+			t_vector OC = subtract(O, C);
+			double a = dot(dir, dir);
+			double b = 2.0 * dot(OC, dir);
+			double c = dot(OC, OC) - r*r;
+
+			// Calculate the discriminant
+			double discriminant = b*b - 4*a*c;
+
+			// If the discriminant is negative, the ray does not intersect the sphere
+			if (discriminant < 0)
+			{
+				// Color the pixel with the background color
+				my_mlx_pixel_put(obj, x, y, COLOR_WHITE);
+			}
+			else
+			{
+				// The ray intersects the sphere, color the pixel with the sphere's color
+				int color = create_trgb(set->spheres[0]->color);
+				my_mlx_pixel_put(obj, x, y, color);
+			}
+		}
+	}
+}
+*/
 void	manage_mlx(t_mlx *obj, t_setting *set)
 {
 	(void)set; //TODO: just for now
@@ -81,6 +122,7 @@ void	manage_mlx(t_mlx *obj, t_setting *set)
 	obj->win = mlx_new_window(obj->mlx, obj->width, obj->height, obj->map_name);
 	my_new_image(obj);
 	//TODO: now draw on the image with some logic
+	//my_test(obj, set);
 	mlx_put_image_to_window(obj->mlx, obj->win, obj->img_pointer, 0, 0);
 	mlx_hook(obj->win, 2, 1, key_pressed, obj);
 	mlx_hook(obj->win, 17, 0L, window_close, obj);
