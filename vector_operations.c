1c1243a8b3d54249ce916bec464a0809ee0d746a
Alessio Tucci <alessiotucci33@gmail.com>
2024-05-12 18:00:14 +0200

    need to check the vector operations

--- vector_operations.c
+++ vector_operations.c
@@ -6,12 +6,12 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 14:48:17 by atucci            #+#    #+#             */
-/*   Updated: 2024/05/12 14:57:01 by atucci           ###   ########.fr       */
+/*   Updated: 2024/05/12 17:59:28 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
-// Function to calculate dot product of two vectors
+/* Function to calculate dot product of two vectors
 double	dot(t_vector v1, t_vector v2)
 {
 	return (v1.x * v2.x + v1.y * v2.y + v1.z * v2.z);
@@ -32,4 +32,55 @@ t_vector	subtract(t_vector v1, t_vector v2)
 int	create_trgb(t_color color)
 {
 	return (0 << 24 | color.r << 16 | color.g << 8 | color.b);
+}
+*/
+//TODO: STUDY THOSE OPERATIONS
+// 1. Vector Addition
+t_vector	add(t_vector v1, t_vector v2)
+{
+	t_vector	result;
+
+	result.x = v1.x + v2.x;
+	result.y = v1.y + v2.y;
+	result.z = v1.z + v2.z;
+	return (result);
+}
+
+// 2. Vector Subtraction
+t_vector	subtract(t_vector v1, t_vector v2)
+{
+	t_vector	result;
+
+	result.x = v1.x - v2.x;
+	result.y = v1.y - v2.y;
+	result.z = v1.z - v2.z;
+	return (result);
+}
+
+// 3. Scalar Multiplication
+t_vector	scalar_mul(t_vector v, double scalar)
+{
+	t_vector	result;
+
+	result.x = v.x * scalar;
+	result.y = v.y * scalar;
+	result.z = v.z * scalar;
+	return (result);
+}
+
+// 4. Dot Product
+double	dot(t_vector v1, t_vector v2)
+{
+	return (v1.x * v2.x + v1.y * v2.y + v1.z * v2.z);
+}
+
+// 5. Cross Product
+t_vector	cross(t_vector v1, t_vector v2)
+{
+	t_vector	result;
+
+	result.x = v1.y * v2.z - v1.z * v2.y;
+	result.y = v1.z * v2.x - v1.x * v2.z;
+	result.z = v1.x * v2.y - v1.y * v2.x;
+	return (result);
 }
