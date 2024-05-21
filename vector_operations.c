2ce3143593b34b62278538a79a3be34d40945708
Alessio Tucci <alessiotucci33@gmail.com>
2024-05-21 16:29:46 +0200

    now it is compiling

--- vector_operations.c
+++ vector_operations.c
@@ -6,27 +6,11 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 14:48:17 by atucci            #+#    #+#             */
-/*   Updated: 2024/05/21 16:28:31 by atucci           ###   ########.fr       */
+/*   Updated: 2024/05/21 16:29:24 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
-// Function to calculate dot product of two vectors
-double	dot(t_vector v1, t_vector v2)
-{
-	return (v1.x * v2.x + v1.y * v2.y + v1.z * v2.z);
-}
-
-// Function to subtract two vectors
-t_vector	subtract(t_vector v1, t_vector v2)
-{
-	t_vector	result;
-
-	result.x = v1.x - v2.x;
-	result.y = v1.y - v2.y;
-	result.z = v1.z - v2.z;
-	return (result);
-}
 
 // Function to convert color struct to int
 int	create_trgb(t_color color)
