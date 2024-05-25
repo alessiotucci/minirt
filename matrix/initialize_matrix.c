656f4ac5ff2b1e256f7d126b995f8e08752c9de2
Alessio Tucci <alessiotucci33@gmail.com>
2024-05-25 19:21:44 +0200

    long work ahead of me

--- matrix/initialize_matrix.c
+++ matrix/initialize_matrix.c
@@ -6,13 +6,13 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 18:49:23 by atucci            #+#    #+#             */
-/*   Updated: 2024/05/25 19:11:15 by atucci           ###   ########.fr       */
+/*   Updated: 2024/05/25 19:21:22 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minirt.h"
 
-static void	copy_value(double destination[4], double source[4])
+void	copy_row_value(double destination[4], double source[4])
 {
 	int	i;
 
@@ -33,20 +33,36 @@ double	**value_matrix(double a[4], double b[4], double c[4], double d[4])
 	i = 0;
 	new = create_matrix(4, 4);
 	init_heap_matrix(4, 4, new);
-	copy_value(new[0], a);
-	copy_value(new[1], b);
-	copy_value(new[2], c);
-	copy_value(new[3], d);
+	copy_row_value(new[0], a);
+	copy_row_value(new[1], b);
+	copy_row_value(new[2], c);
+	copy_row_value(new[3], d);
 	return (new);
 }
 
-int	main()
+int	main(void)
 {
-	double a[4] = {1.0, 2.0, 3.0, 4.0};
-	double b[4] = {5.0, 6.0, 7.0, 8.0};
-	double c[4] = {9.0, 10.0, 11.0, 12.0};
-	double d[4] = {13.0, 14.0, 15.0, 16.0};
+	double	a[4];
+	double	b[4];
+	double	c[4];
+	double	d[4];
 
+	a[0] = 1.0;
+	a[1] = 2.0;
+	a[2] = 3.0;
+	a[3] = 4.0;
+	b[0] = 5.0;
+	b[1] = 6.0;
+	b[2] = 7.0;
+	b[3] = 8.0;
+	c[0] = 9.0;
+	c[1] = 10.0;
+	c[2] = 11.0;
+	c[3] = 12.0;
+	d[0] = 13.0;
+	d[1] = 14.0;
+	d[2] = 15.0;
+	d[3] = 16.0;
 	print_int_matrix(4, 4, value_matrix(a, b, c, d));
-	return 0;
+	return (0);
 }
