0d93597331dc5eef7f7b57860d969f704ea3c543
Alessio Tucci <alessiotucci33@gmail.com>
2024-08-02 17:24:13 +0200

    work done for the day

--- raycasting/cast_ray_helper.c
+++ raycasting/cast_ray_helper.c
@@ -6,7 +6,7 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 15:39:43 by atucci            #+#    #+#             */
-/*   Updated: 2024/08/02 14:54:38 by atucci           ###   ########.fr       */
+/*   Updated: 2024/08/02 17:00:17 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,18 +49,18 @@ void	each_pixel_calculation(t_mlx *data, int x, int y)
 	if (closest_intersection != NULL)
 	{
 		//printf("%sDEBUG%s VERBOSE LOG:\t⚠️ Inside the cast_ray function \n", BG_RED, BG_RESET);
-		printf(" | hit! pixel values: [%s%d, %d%s] |\n", GREEN, x, y, RESET);
+		//printf(" | hit! pixel values: [%s%d, %d%s] |\n", GREEN, x, y, RESET);
 		//print_intersection(*closest_intersection);
-		printf("printing the ray in question:\n");
+		//printf("printing the ray in question:\n");
 		print_ray(ray);
 		t_vector point = position_ray(ray, closest_intersection->t);
 		t_vector normal = v2normal_at(closest_intersection->obj, point);
 		//printf("\t\there, after normal_at v2!\n");
-		t_vector eye = negate(ray.direction);
-		t_material m = material(); // Default material
-		t_color color = lighting(m, *data->setting->lights[0], point, eye, normal);
-		//t_color std = get_color_intersect(closest_intersection->obj);
-		//t_color color = lambert_formula(std, *data->setting->lights[0], point, normal);
+		//t_vector eye = negate(ray.direction);
+		//t_material m = material(); // Default material
+		//t_color color = lighting(m, *data->setting->lights[0], point, eye, normal);
+		t_color std = get_color_intersect(closest_intersection->obj);
+		t_color color = lambert_formula(std, *data->setting->lights[0], point, normal);
 		//print_color(color);
 		my_mlx_pixel_put(data, x, y, create_trgb(color));
 		//my_mlx_pixel_put(data, x, y, calculate_sphere_color(closest_intersection));
@@ -93,9 +93,11 @@ void	each_pixel_calculationV2(t_mlx *data, int x, int y)
 		print_ray(ray);
 		t_vector point = position_ray(ray, closest_intersection->t);
 		t_vector normal = v2normal_at(closest_intersection->obj, point);
-		t_vector eye = negate(ray.direction);
-		t_material m = material(); // Default material
-		t_color color = lighting(m, *data->setting->lights[0], point, eye, normal);
+		//t_vector eye = negate(ray.direction);
+		//t_material m = material(); // Default material
+		//t_color color = lighting(m, *data->setting->lights[0], point, eye, normal);
+		t_color std = get_color_intersect(closest_intersection->obj);
+		t_color color = lambert_formula(std, *data->setting->lights[0], point, normal);
 		my_mlx_pixel_put(data, x, y, create_trgb(color));
 	}
 	else
