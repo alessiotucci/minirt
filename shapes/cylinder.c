444250a8ca03149811284222cac6cdab739ae549
atucci <alessiotucci33@gmail.com>
2024-12-15 17:36:40 +0100

    boilerplate code, my type

--- shapes/cylinder.c
+++ shapes/cylinder.c
@@ -6,11 +6,13 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 19:26:30 by atucci            #+#    #+#             */
-/*   Updated: 2024/12/13 19:29:08 by atucci           ###   ########.fr       */
+/*   Updated: 2024/12/15 17:36:13 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minirt.h"
+#include <float.h>
+//TODO: check if this library is allowed
 
 t_cylinder	create_cylinder(char *id, t_vector center, double d, t_color c)
 {
@@ -27,5 +29,8 @@ t_cylinder	create_cylinder(char *id, t_vector center, double d, t_color c)
 	new_cylinder.color = c;
 	new_cylinder.transform = def;
 	new_cylinder.material = material();
+//	last update...
+	new_cylinder.min = DBL_MIN;
+	new_cylinder.max = DBL_MAX;
 	return (new_cylinder);
 }
