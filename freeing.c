8b81cee11ffc365c12458d9dac15ae457208d111
Alessio Tucci <alessiotucci33@gmail.com>
2025-02-01 18:35:10 +0100

    up

--- freeing.c
+++ freeing.c
@@ -6,7 +6,7 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 19:35:20 by atucci            #+#    #+#             */
-/*   Updated: 2024/12/20 18:36:41 by atucci           ###   ########.fr       */
+/*   Updated: 2025/02/01 18:34:35 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,12 +104,7 @@ void	free_cylinder_array(t_cylinder **cylinders, int num)
 void	free_struct(t_setting *set)
 {
 	printf("%sRemember to free all the memory%s\n", RED, RESET);
-	//free(set->amb_light->identifier);
-	//free(set->camera->identifier);
-	//fee(set->light->identifier);
-	//free(set->amb_light);
-	//free(set->camera);
-	//free(set->light);
+	printf("%sUPDATE 1 Feb: fixing cylinder%s\n", RED, RESET);
 	if (set->amb_light)
 	{
 		free(set->amb_light->identifier);
@@ -125,4 +120,3 @@ void	free_struct(t_setting *set)
 	free_plane_array(set->planes, set->num_planes);
 	free_cylinder_array(set->cylinders, set->num_cylinders);
 }
-// ok
