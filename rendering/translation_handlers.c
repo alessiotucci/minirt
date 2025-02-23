b65a9108dcab2231e5d5fc99cbde8ebb20a14dc0
Alessio Tucci <alessiotucci33@gmail.com>
2025-02-23 16:10:13 +0100

    I am norminetting right now

--- rendering/translation_handlers.c
+++ rendering/translation_handlers.c
@@ -6,14 +6,14 @@
 /*   By: atucci <atucci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 20:23:32 by atucci            #+#    #+#             */
-/*   Updated: 2025/02/18 18:33:32 by atucci           ###   ########.fr       */
+/*   Updated: 2025/02/23 16:09:35 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minirt.h"
 
 // Clamps a vector's magnitude to a maximum value.
-t_vector clamp_delta(t_vector delta, double max_val)
+t_vector	clamp_delta(t_vector delta, double max_val)
 {
 	double	mag;
 	double	factor;
@@ -39,22 +39,17 @@ t_vector	get_selected_object_position(t_mlx *mlx)
 	else if (mlx->selected.type == T_LIGHT)
 		pos = mlx->setting->lights[mlx->selected.index]->position;
 	else
-	{
-		printf("Translation not supported for this object type.\n");
 		pos = create_vector(0, 0, 0);
-	}
 	return (pos);
 }
 
-// Create a ray through the mouse coordinates using the improved camera function.
-// Define the reference plane:
-//  - Its point is the object's current position.
-//  - Its normal is the camera's orientation.
-// The plane equation is: (ray.origin + t*ray.direction - current_position) dot cam->orientation = 0.
-// Solve for t: t = -((ray.origin - current_position) dot cam->orientation) / (ray.direction dot cam->orientation)
-// Compute the target point on the plane.
-// Compute the delta vector from the object's current position to the target.
-// Optionally apply a sensitivity factor and clamp the delta.
+//Create a ray through the mouse coordinates using the improved camera function
+//Define the reference plane:
+// - Its point is the object's current position
+// - Its normal is the camera's orientation
+//Compute the target point on the plane
+//Compute the delta vector from the object's current position to the target
+//Optionally apply a sensitivity factor and clamp the delta
 t_vector	calculate_translation_delta_perp_to_camera(t_mlx *mlx, int x, int y, t_vector current_position)
 {
 	t_ray		ray;
@@ -90,52 +85,48 @@ t_vector	calculate_translation_delta(t_mlx *mlx, int x, int y, double reference_
 	t_vector	delta;
 
 	ray = create_ray_from_camera2(mlx, x, y);
-
 	if (fabs(ray.direction.y) < EPSILON)
-		return create_vector(0, 0, 0);
+		return (create_vector(0, 0, 0));
 	t = (reference_y - ray.origin.y) / ray.direction.y;
 	target = add(ray.origin, multiplication(ray.direction, t));
 	delta = subtract(target, current_position);
-    // Apply sensitivity factor.
-    delta = multiplication(delta, TRANSLATION_SENSITIVITY);
-    // Clamp the delta's magnitude.
-    delta = clamp_delta(delta, MAX_DELTA);
-	return delta;
+	delta = multiplication(delta, TRANSLATION_SENSITIVITY);
+	delta = clamp_delta(delta, MAX_DELTA);
+	return (delta);
 }
 
 // Helper function that computes and applies a translation delta based on mouse x, y.
-void perform_translation_from_mouse(t_mlx *mlx, int x, int y)
+void	perform_translation_from_mouse(t_mlx *mlx, int x, int y)
 {
 	t_vector	current_position;
-	double		reference_y;(void)reference_y;
+	double		reference_y;
 	t_vector	delta;
 
 	current_position = get_selected_object_position(mlx);
 	reference_y = current_position.y;
-	//delta = calculate_translation_delta(mlx, x, y, reference_y, current_position);
 	delta = calculate_translation_delta_perp_to_camera(mlx, x, y, current_position);
 	translate_object(mlx, delta);
-	printf("\n\nTranslated object by delta: (%.2f, %.2f, %.2f)\n", delta.x, delta.y, delta.z);
 }
 
 void	translate_object(t_mlx *data, t_vector delta)
 {
+	t_sphere	*sp;
+	t_cylinder	*cy;
+	t_light		*light;
+
 	if (data->selected.type == T_SPHERE)
 	{
-		t_sphere *sp = data->setting->spheres[data->selected.index];
+		sp = data->setting->spheres[data->selected.index];
 		sp->center = add(sp->center, delta);
-		printf("Sphere center updated to: (%lf, %lf, %lf)\n", sp->center.x, sp->center.y, sp->center.z);
 	}
 	else if (data->selected.type == T_CYLINDER)
 	{
-		t_cylinder *cy = data->setting->cylinders[data->selected.index];
+		cy = data->setting->cylinders[data->selected.index];
 		cy->center = add(cy->center, delta);
-		printf("Cylinder center updated to: (%lf, %lf, %lf)\n", cy->center.x, cy->center.y, cy->center.z);
 	}
 	else if (data->selected.type == T_LIGHT)
 	{
-		t_light *light = data->setting->lights[data->selected.index];
+		light = data->setting->lights[data->selected.index];
 		light->position = add(light->position, delta);
-		printf("Light position updated to: (%lf, %lf, %lf)\n", light->position.x, light->position.y, light->position.z);
 	}
 }
